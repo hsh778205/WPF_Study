4ac1f50f15897d435da734190542be2b1140b443
hsh778205 <heshaohong2015@outlook.com>
2024-01-20 13:32:58 +0800

    07定义Style样式与样式的继承

---
+++ WPF_Study/07定义Style样式与样式的继承.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Study
+{
+    /// <summary>
+    /// _07定义Style样式与样式的继承.xaml 的交互逻辑
+    /// </summary>
+    public partial class _07定义Style样式与样式的继承 : Window
+    {
+        public _07定义Style样式与样式的继承()
+        {
+            InitializeComponent();
+        }
+    }
+}
