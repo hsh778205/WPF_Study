12fe45f546673c8964aa2edb8cfc0ff62d4ba008
hsh778205 <heshaohong2015@outlook.com>
2024-02-02 16:19:30 +0800

    18 WPF_Study_LIBRARY MVVM-命令模式的实现与应用

---
+++ WPF_Study_LIBRARY/LoginVM.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WPF_Study_LIBRARY;
+
+namespace WPF_Study
+{
+    public class LoginVM : INotifyPropertyChanged
+    {
+        private MainWindow _main;
+        public LoginVM(MainWindow main)
+        {
+            _main = main;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyChanged)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyChanged));
+        }
+
+        private LoginModel _loginModel = new LoginModel();
+
+        public string UserName
+        {
+            get { return _loginModel.UserName; }
+            set
+            {
+                _loginModel.UserName = value;
+                RaisePropertyChanged("UserName");
+            }
+        }
+
+        public string Password
+        {
+            get { return _loginModel.Password; }
+            set
+            {
+                _loginModel.Password = value;
+                RaisePropertyChanged("Password");
+            }
+        }
+
+        void LoginFunc()
+        {
+            if (UserName == "wpf" && Password == "777")
+            {
+                //MessageBox.Show("Login");
+                Index index = new Index();
+                index.Show();
+                _main.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Error");
+                UserName = "";
+                Password = "";
+            }
+        }
+        bool CanLoginExecute()
+        {
+            return true;
+        }
+        public ICommand LoginAction
+        {
+            get
+            {
+                return new RelayCommand(LoginFunc, CanLoginExecute);
+            }
+        }
+
+
+
+
+    }
+}
---
+++ WPF_Study_LIBRARY/RelayCommand.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_Study_LIBRARY
+{
+    public class RelayCommand:ICommand
+    {
+        /// <summary>
+        /// 命令是否能够执行
+        /// </summary>
+        readonly Func<bool> _canExecute;
+        /// <summary>
+        /// 命令需要执行的方法
+        /// </summary>
+        readonly Action _execute;
+
+        public RelayCommand(Action action,Func<bool> canExecute)
+        {
+            _canExecute = canExecute;
+            _execute = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+            {
+                return true;
+            }
+            return _canExecute();
+        }
+
+
+        public void Execute(object parameter)
+        {
+            if (_execute == null)
+            {
+                return;
+            }
+            _execute();
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (_canExecute != null)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+            remove
+            {
+                if (_canExecute != null)
+                {
+                    CommandManager.RequerySuggested -= value;
+                }
+            }
+        }
+    }
+}
